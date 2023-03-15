77218515263702cbafb0ea2e4cb477dd8e8d1d14
Shivam Dhuria <shivam.dhuria07@gmail.com>
2023-03-15 17:36:38 +0530

    first commit

--- app/src/main/java/com/elixer/list/MainActivityNew.kt
+++ app/src/main/java/com/elixer/list/MainActivityNew.kt
@@ -6,7 +6,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -18,13 +17,11 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.key
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.elixer.list.ui.theme.ListTheme
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.media3.common.util.UnstableApi
@@ -43,27 +40,13 @@ class MainActivityNew : ComponentActivity() {
 
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
 
-          var movieList = remember {
-            mutableStateListOf(
-              mock4.get(0),
-              mock4.get(1),
-              mock4.get(2),
-              mock4.get(3),
-              mock4.get(4),
-              mock4.get(5),
-            )
-          }
-
+          var movieList = remember { mutableStateListOf(mock4.get(0), mock4.get(1), mock4.get(2), mock4.get(3), mock4.get(4), mock4.get(5),) }
           fun onClick() {
             movieList.removeLast()
           }
 
           Column {
-            LogCompositions(tag = "Swipe", msg = "Column")
-            Spacer(modifier = Modifier.height(100.dp))
-            ScopedView {
-              ContentList(movieList.takeLast(2))
-            }
+            ContentList(movieList.takeLast(2))
             Button(
               onClick = ::onClick
             ) {
@@ -77,26 +60,15 @@ class MainActivityNew : ComponentActivity() {
 
   @Composable
   private fun ContentList(movieList: List<GameEntry>) {
-
     Box {
-      LogCompositions(tag = "Swipe", msg = "ContentList Box ")
-
+      LogCompositions(tag = "", msg = "ContentList Box ")
       ScopedView {
-
-
         movieList.forEachIndexed { index, movie ->
-          Log.d("Swipe", "Index ${index} movie id-> ${movie?.id}")
-          //This box causes recomp/disposing exoplayer, while fixing on top ossie
-//      Box() {
-//        key(movie.id) {
-//          Box() {
           key(movie.id) {
-            movie?.let { COntentView(Modifier.fillMaxWidth(), it, index == 1) }
-
+            ContentView(
+              Modifier.fillMaxWidth(), movie, index == 1
+            )
           }
-//          }
-//        }
-//      }
         }
       }
     }
@@ -104,14 +76,16 @@ class MainActivityNew : ComponentActivity() {
 }
 
 @Composable
-private fun COntentView(modifier: Modifier, movie: GameEntry, isActive: Boolean) {
+private fun ContentView(modifier: Modifier, movie: GameEntry, isActive: Boolean) {
   Card(
     modifier
       .height(400.dp)
       .padding(horizontal = 30.dp)
   ) {
-    LogCompositions(tag = "Swipe", msg = "ContentView id ${movie.id}")
-    veedi(videoUri = movie.media?.url.toString(), id = movie.id, isActive)
+
+      MediaPlayer(videoUri = movie.media?.url.toString(), id = movie.id, isActive)
+
+    // The below text Composable works as expected
     Text(
       text = movie.id.toString(),
       textAlign = TextAlign.Center,
--- app/src/main/java/com/elixer/list/MediaPlayer.kt
+++ app/src/main/java/com/elixer/list/MediaPlayer.kt
@@ -1,48 +1,38 @@
-package com.elixer.list
+package com.elixer.list.ui
 
 import android.util.Log
-import android.view.ViewGroup.LayoutParams.*
+import android.view.SurfaceView
+import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.compose.ui.zIndex
-import androidx.core.content.ContentProviderCompat.*
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.media3.common.C
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
-import androidx.media3.common.Player.*
-import androidx.media3.datasource.DataSource
 import androidx.media3.datasource.DefaultDataSource
-import androidx.media3.datasource.DefaultDataSourceFactory
 import androidx.media3.datasource.DefaultHttpDataSource
 import androidx.media3.datasource.cache.CacheDataSource
-import androidx.media3.exoplayer.DefaultLoadControl
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.exoplayer.source.MediaSource
 import androidx.media3.exoplayer.source.ProgressiveMediaSource
-import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
 import androidx.media3.ui.AspectRatioFrameLayout
 import androidx.media3.ui.PlayerView
+import com.elixer.list.CultApplication
+import com.elixer.list.LogCompositions
 
-
-@Composable
 @androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
-fun VideoPlayerCard(videoUri: String, shouldPlayBack: Boolean = false) {
+@Composable
+fun MediaPlayer(videoUri: String, id: Long, isActive:Boolean) {
   val context = LocalContext.current
   val application = context.applicationContext as CultApplication
-  val lifecycleOwner = rememberUpdatedState(LocalLifecycleOwner.current)
 
   val exoPlayer = remember {
     ExoPlayer.Builder(context).build()
@@ -64,43 +54,12 @@ fun VideoPlayerCard(videoUri: String, shouldPlayBack: Boolean = false) {
             .createMediaSource(mediaItem)
         }
         mediaSource?.let { setMediaSource(it, true) }
-        playWhenReady = false
+        playWhenReady = true
         videoScalingMode = C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING
         repeatMode = Player.REPEAT_MODE_ONE
         prepare()
       }
   }
-
-
-  LogCompositions(tag = "Swipe", msg = "Video Card -> ${videoUri} ..")
-  exoPlayer.addListener(object : Player.Listener {
-
-    override fun onPlaybackStateChanged(state: Int) {
-      when (state) {
-        STATE_ENDED -> {
-          exoPlayer.seekTo(0L)
-          Log.e("onPlaybackStateChanged", "ended")
-        }
-
-        STATE_IDLE -> {
-//                    exoPlayer.pause()
-          Log.e("onPlaybackStateChanged", "idle")
-
-        }
-
-        STATE_READY -> {
-//                    exoPlayer.pause()
-          Log.e("onPlaybackStateChanged", "ready")
-
-        }
-      }
-    }
-  })
-  LaunchedEffect(shouldPlayBack) {
-    exoPlayer.playWhenReady = shouldPlayBack
-  }
-
-
 
   DisposableEffect(
     AndroidView(
@@ -110,32 +69,26 @@ fun VideoPlayerCard(videoUri: String, shouldPlayBack: Boolean = false) {
           useController = false
           resizeMode = AspectRatioFrameLayout.RESIZE_MODE_ZOOM
           player = exoPlayer
-          layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+          layoutParams = FrameLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+          )
+          videoSurfaceView?.alpha = 0.5f
+          videoSurfaceView?.translationZ = 40f
+          (videoSurfaceView as? SurfaceView)?.let {
+            Log.d("!!!!Surface view", "id ${id}")
+            it.setZOrderOnTop(true)
+            it.setZOrderMediaOverlay(false)
+            Log.d("!!!!Surface view", "id ${id} hasOverlappingRendering " +
+                    "${hasOverlappingRendering}")
+
+          }
         }
       },
     )
   ) {
-    Log.d("Swipe", "exoplayer created ${videoUri}")
-
-    val observer = LifecycleEventObserver { owner, event ->
-      when (event) {
-        Lifecycle.Event.ON_PAUSE -> {
-//                    exoPlayer.pause()
-        }
-
-        Lifecycle.Event.ON_RESUME -> {
-//                    exoPlayer.play()
-        }
-
-        else -> {}
-      }
-    }
-    val lifecycle = lifecycleOwner.value.lifecycle
-    lifecycle.addObserver(observer)
     onDispose {
-      Log.d("Swipe", "exoplayer disposed ${videoUri}")
       exoPlayer.release()
-      lifecycle.removeObserver(observer)
     }
   }
 }
