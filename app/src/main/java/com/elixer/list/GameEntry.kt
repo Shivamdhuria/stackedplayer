af5a7ff2e71775016fa873a1e239efaba8c02b2e
Shivam Dhuria <shivam.dhuria07@gmail.com>
2023-03-17 15:13:43 +0530

    added swipe but exop gets recomp

--- app/src/main/java/com/elixer/list/GameEntry.kt
+++ app/src/main/java/com/elixer/list/GameEntry.kt
@@ -1,42 +1,54 @@
 package com.elixer.list
 
 import android.util.Log
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
+import com.alexstyl.swipeablecard.Direction
+import com.alexstyl.swipeablecard.SwipeableCardState
 import com.elixer.list.mediaCompose.media.Media
-import com.elixer.list.mediaCompose.media.ShowBuffering
 import com.elixer.list.mediaCompose.media.SurfaceType
 import com.elixer.list.mediaCompose.media.rememberMediaState
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player.REPEAT_MODE_ONE
-import com.google.android.exoplayer2.Player.RepeatMode
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.upstream.DefaultDataSource
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
 import com.google.android.exoplayer2.upstream.cache.CacheDataSource
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlin.math.abs
 
 
 data class GameEntry(
@@ -92,15 +104,15 @@ val mockMovies = listOf(
       url = "https://cdn-cult.s3.us-west-2.amazonaws.com/media/ebb81dea-d559-4c25-8793-fe99f9a3a746.mp4",
     ),
     mediaId = 17,
-  ), //  GameEntry(
-  //    authorId = 1693470030420771857, id = 3, instanceId = 1697052293175706907, invitationId = null,
-  //    media = Media(
-  //      id = 3, //
-  //       url = "https://cdn-cult.s3.us-west-2.amazonaws.com/media/958656f7-949b-4b25-b4a3-9ea9ad82212b.mp4",
-  ////      url = "https://html5demos.com/assets/dizzy.mp4",
-  //    ),
-  //    mediaId = 17,
-  //  ),
+  ),
+  GameEntry(
+    authorId = 1693470030420771857, id = 3, instanceId = 1697052293175706907, invitationId = null,
+    media = Media(
+      id = 3, //
+      url = "https://cdn-cult.s3.us-west-2.amazonaws.com/media/958656f7-949b-4b25-b4a3-9ea9ad82212b.mp4", //      url = "https://html5demos.com/assets/dizzy.mp4",
+    ),
+    mediaId = 17,
+  ),
   GameEntry(
     authorId = 1693470030420771857, id = 4, instanceId = 1697052293175706907, invitationId = null,
     media = Media(
@@ -113,7 +125,7 @@ val mockMovies = listOf(
   GameEntry(
     authorId = 1693470030420771857, id = 5, instanceId = 1697052293175706907, invitationId = null,
     media = Media(
-      id = 6, //
+      id = 5, //
       url = "https://cdn-cult.s3.us-west-2.amazonaws.com/media/b71bb799-4fd4-4c24-b9c0-fd064de3a22d.mp4", //      url = "https://storage.googleapis.com/downloads.webmproject.org/av1/exoplayer/bbb-av1-480p.mp4",
     ),
     mediaId = 17,
@@ -123,30 +135,144 @@ val mockMovies = listOf(
 
 
 @Composable
-fun ContentList(modifier: Modifier = Modifier, movieList: List<GameEntry>, onRemove: () -> Unit) {
+fun ContentStack(modifier: Modifier = Modifier, movieList: List<GameEntry>, onRemove: () -> Unit) {
 
-  val mediaItems = remember(movieList) {
-    movieList.map {
-      MediaItem.Builder().setMediaId(it.media?.url.toString()).setUri(it.media?.url.toString()).build()
-    }
+  //  val mediaItems = remember(movieList) {
+  //    movieList.map {
+  //      MediaItem.Builder().setMediaId(it.media?.url.toString()).setUri(it.media?.url.toString()).build()
+  //    }
+  //  }
+
+  val gameEntry = remember() { mutableStateListOf<GameEntry>() }
+  Log.e("TAG GameEntry", gameEntry.map { it.id }.toString())
+
+  var currentMovie by remember { mutableStateOf<GameEntry?>(null) }
+
+  val screenWidth = with(LocalDensity.current) { LocalConfiguration.current.screenWidthDp.dp.toPx() }
+  val screenHeight = with(LocalDensity.current) { LocalConfiguration.current.screenHeightDp.dp.toPx() }
+  val scope = rememberCoroutineScope() //    var activeCardState = rememberSwipeableCardState() //    val queuedCardState = rememberSwipeableCardState()
+  val activeOffset by remember {
+    mutableStateOf(
+      Animatable(
+        Offset.Zero, Offset.VectorConverter
+      )
+    )
   }
 
-  Log.e("TAG", mediaItems.map{it.mediaId.toString()}.toString())
+  fun onSwipedNew(direction: Direction) {
+    onRemove()
+  }
+
+
+  suspend fun swipeNew(direction: Direction, animationSpec: AnimationSpec<Offset> = tween(400)) {
+    val endX = screenWidth * 1.5f
+    val endY = screenHeight
+    when (direction) {
+      Direction.Left -> activeOffset.animateTo(Offset(x = -endX, 0f), animationSpec)
+      Direction.Right -> activeOffset.animateTo(Offset(x = endX, 0f), animationSpec)
+      Direction.Up -> activeOffset.animateTo(Offset(x = 0f, y = -endY), animationSpec)
+      Direction.Down -> activeOffset.animateTo(Offset(x = 0f, y = endY), animationSpec)
+    }
+    onSwipedNew(direction)
+  }
+
+  LaunchedEffect(movieList) { //
+    Log.e("TAG MOVIE LIST rec", gameEntry.map { it.id }.toString())
+
+    if (gameEntry.isEmpty()) {
+      gameEntry.addAll(movieList)
+    } else { //
+       gameEntry.removeLast()
+      movieList.forEach {
+        if (!gameEntry.contains(it)) {
+          gameEntry.add(0, it)
+        }
+      }
+    }
+    activeOffset.snapTo(Offset(0f, 0f))
+
+    currentMovie = movieList.last() //    activeOffset.snapTo(Offset(0f, 0f))
+  }
+
+  //  Log.e("TAG", mediaItems.map { it.mediaId.toString() }.toString())
   Box(
     modifier = modifier,
   ) {
-    mediaItems.forEachIndexed { index, mediaItem ->
-      Log.e("TAG", "media item -- > ${mediaItem.mediaId}, index -> ${index},")
-//      key(mediaItem.mediaId) {
-        ListItemNew(
-          modifier = Modifier
-            .clickable(onClick = { onRemove() })
-            .padding(horizontal = 40.dp)
-            .aspectRatio(9 / 16f),
-          showVideo = mediaItem == mediaItems.last(),
-          mediaItem = mediaItem,
-        ) {}
-//      }
+    gameEntry.forEachIndexed { index, mediaItem ->
+      Log.e("TAG", "media item -- > ${mediaItem.id}, index -> ${index},") //      key(mediaItem.mediaId) {
+      ScopedView {
+        key(mediaItem.id) {
+
+          SwipeableCard(
+            modifier = Modifier
+              .padding(horizontal = 40.dp)
+              .aspectRatio(9 / 16f)
+              .pointerInput(Unit) {
+                coroutineScope {
+                  detectDragGestures(onDragCancel = {
+                    launch { //                    state.reset()
+                      //                    onSwipeCancel()
+                    }
+                  }, onDrag = { change, dragAmount ->
+                    launch {
+                      val original = activeOffset.targetValue
+                      val summed = original + dragAmount
+                      val newValue = Offset(
+                        x = summed.x.coerceIn(-screenWidth, screenWidth), y = summed.y.coerceIn(-screenHeight, screenHeight)
+                      )
+                      if (change.positionChange() != Offset.Zero) change.consume()
+                      activeOffset.animateTo(Offset(newValue.x, newValue.y))
+                    }
+                  }, onDragEnd = {
+                    launch {
+                      val coercedOffset = activeOffset.targetValue.coerceIn(
+                        listOf(Direction.Up, Direction.Down), maxHeight = screenHeight, maxWidth = screenWidth
+                      )
+
+                      if (hasNotTravelledEnoughNew(
+                          screenWidth, screenHeight, coercedOffset
+                        )
+                      ) {
+                        activeOffset.animateTo(Offset.Zero, tween(400))
+                      } else {
+                        val horizontalTravel = abs(activeOffset.targetValue.x)
+                        val verticalTravel = abs(activeOffset.targetValue.y)
+
+                        if (horizontalTravel > verticalTravel) {
+                          if (activeOffset.targetValue.x > 0) {
+                            swipeNew(Direction.Right) //                          onSwiped(Direction.Right)
+                          } else {
+                            swipeNew(Direction.Left) //                          onSwiped(Direction.Left)
+                          }
+                        } else {
+                          if (activeOffset.targetValue.y < 0) {
+                            swipeNew(Direction.Up) //                          onSwiped(Direction.Up)
+                          } else {
+                            swipeNew(Direction.Down) //                          onSwiped(Direction.Down)
+                          }
+                        }
+                      }
+                    }
+                  })
+                }
+              }
+              .graphicsLayer {
+                if (mediaItem == currentMovie) {
+                  translationX = activeOffset.value.x
+                  translationY = activeOffset.value.y
+                  rotationZ = (activeOffset.value.x / 60).coerceIn(-40f, 40f)
+                } else {
+                  translationX = 0f
+                  translationY = 0f
+                  rotationZ = 0f
+                }
+
+              },
+            showVideo = mediaItem == currentMovie,
+            mediaItem = mediaItem,
+          ) {}
+        }
+      } //      }
     }
   }
 
@@ -159,9 +285,12 @@ fun ContentList(modifier: Modifier = Modifier, movieList: List<GameEntry>, onRem
 }
 
 @Composable
-fun ListItemNew(
-  modifier: Modifier, showVideo: Boolean, mediaItem: MediaItem, video: @Composable BoxScope.() -> Unit
+fun SwipeableCard(
+  modifier: Modifier, showVideo: Boolean, mediaItem: GameEntry, video: @Composable BoxScope.() -> Unit
 ) {
+
+  val mediaSourceUrl = MediaItem.Builder().setMediaId(mediaItem.media?.url.toString()).setUri(mediaItem.media?.url.toString()).build()
+
   val player by rememberManagedExoPlayer()
   player?.apply {
     val httpDataSourceFactory: DefaultHttpDataSource.Factory = DefaultHttpDataSource.Factory().setAllowCrossProtocolRedirects(true)
@@ -170,7 +299,7 @@ fun ListItemNew(
       CacheDataSource.Factory().setCache(it).setUpstreamDataSourceFactory(defaultDataSourceFactory).setFlags(CacheDataSource.FLAG_IGNORE_CACHE_ON_ERROR)
     }
     val mediaSource: ProgressiveMediaSource? = cacheDataSourceFactory?.let {
-      mediaItem.let { it1 ->
+      mediaSourceUrl.let { it1 ->
         ProgressiveMediaSource.Factory(it).createMediaSource(it1)
       }
     }
@@ -185,14 +314,14 @@ fun ListItemNew(
       player?.pause()
     }
   }
+
   Card(
     modifier = modifier, shape = RoundedCornerShape(12.dp)
   ) {
     Box(modifier = Modifier) {
       Media(
-        state = rememberMediaState(player = player), modifier = Modifier
-          .fillMaxSize()
-          .background(Color.Black),
+        state = rememberMediaState(player = player),
+        modifier = Modifier.fillMaxSize(),
         surfaceType = SurfaceType.TextureView,
       )
     }
@@ -230,4 +359,42 @@ fun ListItem(
       }
     }
   }
+}
+
+public fun Offset.coerceIn(
+  blockedDirections: List<Direction>,
+  maxHeight: Float,
+  maxWidth: Float,
+): Offset {
+  return copy(
+    x = x.coerceIn(
+      if (blockedDirections.contains(Direction.Left)) {
+        0f
+      } else {
+        -maxWidth
+      }, if (blockedDirections.contains(Direction.Right)) {
+        0f
+      } else {
+        maxWidth
+      }
+    ), y = y.coerceIn(
+      if (blockedDirections.contains(Direction.Up)) {
+        0f
+      } else {
+        -maxHeight
+      }, if (blockedDirections.contains(Direction.Down)) {
+        0f
+      } else {
+        maxHeight
+      }
+    )
+  )
+}
+
+public fun hasNotTravelledEnoughNew(
+  maxHeight: Float,
+  maxWidth: Float,
+  offset: Offset,
+): Boolean {
+  return abs(offset.x) < maxWidth / 4 && abs(offset.y) < maxHeight / 4
 }
--- app/src/main/java/com/elixer/list/MainActivityNew.kt
+++ app/src/main/java/com/elixer/list/MainActivityNew.kt
@@ -7,10 +7,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -25,16 +23,13 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.zIndex
 import com.elixer.list.mediaCompose.media.Media
 import com.elixer.list.mediaCompose.media.rememberMediaState
 import com.elixer.list.ui.theme.ListTheme
@@ -50,10 +45,9 @@ class MainActivityNew : ComponentActivity() {
     super.onCreate(savedInstanceState)
     setContent {
 
-//      InsideListContent(Modifier.fillMaxSize())
+      //      InsideListContent(Modifier.fillMaxSize())
 
-      ListTheme {
-        // A surface container using the 'background' color from the theme
+      ListTheme { // A surface container using the 'background' color from the theme
 
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
 
@@ -64,7 +58,7 @@ class MainActivityNew : ComponentActivity() {
               mockMovies.get(2),
               mockMovies.get(3),
               mockMovies.get(4),
-//              mockMovies.get(5),
+              mockMovies.get(5),
             )
           }
 
@@ -78,8 +72,11 @@ class MainActivityNew : ComponentActivity() {
             ) {
               Text(text = "remove last")
             }
-            ContentList(Modifier, movieList.takeLast(2)){
-              onClick()
+            ScopedView {
+
+              ContentStack(Modifier, movieList.takeLast(2)) {
+                onClick()
+              }
             }
             Button(
               onClick = ::onClick
@@ -94,12 +91,14 @@ class MainActivityNew : ComponentActivity() {
 
 
 }
+
 val Urls = listOf(
   "https://storage.googleapis.com/downloads.webmproject.org/av1/exoplayer/bbb-av1-480p.mp4",
   "https://storage.googleapis.com/exoplayer-test-media-1/mkv/android-screens-lavf-56.36.100-aac-avc-main-1280x720.mkv",
   "https://storage.googleapis.com/exoplayer-test-media-1/mp4/frame-counter-one-hour.mp4",
   "https://html5demos.com/assets/dizzy.mp4",
 )
+
 @Composable
 fun InsideListContent(
   modifier: Modifier = Modifier,
@@ -133,15 +132,11 @@ fun InsideListContent(
             val httpDataSourceFactory: DefaultHttpDataSource.Factory = DefaultHttpDataSource.Factory().setAllowCrossProtocolRedirects(true)
             val defaultDataSourceFactory: DefaultDataSource.Factory = DefaultDataSource.Factory(application, httpDataSourceFactory)
             val cacheDataSourceFactory: CacheDataSource.Factory? = application.simpleCache?.let {
-              CacheDataSource.Factory()
-                .setCache(it)
-                .setUpstreamDataSourceFactory(defaultDataSourceFactory)
-                .setFlags(CacheDataSource.FLAG_IGNORE_CACHE_ON_ERROR)
+              CacheDataSource.Factory().setCache(it).setUpstreamDataSourceFactory(defaultDataSourceFactory).setFlags(CacheDataSource.FLAG_IGNORE_CACHE_ON_ERROR)
             }
             val mediaSource: ProgressiveMediaSource? = cacheDataSourceFactory?.let {
               mediaItem.let { it1 ->
-                ProgressiveMediaSource.Factory(it)
-                  .createMediaSource(it1)
+                ProgressiveMediaSource.Factory(it).createMediaSource(it1)
               }
             }
             mediaSource?.let { (this as? ExoPlayer)?.setMediaSource(it, true) }
@@ -149,8 +144,7 @@ fun InsideListContent(
           }
         }
         Media(
-          state = rememberMediaState(player = player),
-          modifier = Modifier
+          state = rememberMediaState(player = player), modifier = Modifier
             .matchParentSize()
             .background(Color.Black)
         )
@@ -158,6 +152,7 @@ fun InsideListContent(
     }
   }
 }
+
 @Composable
 fun ContentView(movie: GameEntry, isActive: Boolean) {
 
@@ -167,24 +162,19 @@ fun ContentView(movie: GameEntry, isActive: Boolean) {
       .padding(horizontal = 50.dp)
       .padding(vertical = 30.dp)
   ) {
-    LogCompositions("ContentView ${movie.id}")
-////    key(movie.media?.url) {
-//    MediaPlayer(
-//      Modifier.height(200.dp), videoUri = movie.media?.url
-//        .toString
-//          (), id = movie.id,
-//      isActive
-//    )
-//
-////    }
+    LogCompositions("ContentView ${movie.id}") ////    key(movie.media?.url) {
+    //    MediaPlayer(
+    //      Modifier.height(200.dp), videoUri = movie.media?.url
+    //        .toString
+    //          (), id = movie.id,
+    //      isActive
+    //    )
+    //
+    ////    }
 
     // The below text Composable works as expected
     Text(
-      text = movie.id.toString(),
-      textAlign = TextAlign.Center,
-      color = Color.White,
-      fontSize = 20.sp,
-      modifier = Modifier.fillMaxWidth()
+      text = movie.id.toString(), textAlign = TextAlign.Center, color = Color.White, fontSize = 20.sp, modifier = Modifier.fillMaxWidth()
     )
   }
   DisposableEffect(Unit) {
